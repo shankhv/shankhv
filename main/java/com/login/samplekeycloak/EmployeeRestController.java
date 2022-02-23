f787c3290d8788ffc8f749e8badb0be6b6e72bbe
shank <shashankhv30@gmail.com>
2022-02-23 14:37:38 +0530

    keycloak to OAuth2 Connector

---
+++ main/java/com/login/samplekeycloak/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.login.samplekeycloak;
+
+import java.util.Set;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeRestController {
+	  @GetMapping(path = "/username")
+	  @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+	  public ResponseEntity<String> getAuthorizedUserName() {
+	    return ResponseEntity.ok(SecurityContextUtils.getUserName());
+	  }
+
+	  @GetMapping(path = "/roles")
+	  @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+	  public ResponseEntity<Set<String>> getAuthorizedUserRoles() {
+	    return ResponseEntity.ok(SecurityContextUtils.getUserRoles());
+	  }
+}
---
+++ test/java/com/login/samplekeycloak/SamplekeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.samplekeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplekeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
